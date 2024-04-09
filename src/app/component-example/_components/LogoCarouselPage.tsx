491d9873270f0e1f5430d82a3bf7fcb214fc99e6
Kelvin <ykekelvin0220@gmail.com>
2024-04-09 08:37:18 +0800

    chore: code enhancement

--- src/app/component-example/_components/LogoCarouselPage.tsx
+++ src/app/component-example/_components/LogoCarouselPage.tsx
@@ -1,13 +1,35 @@
 import LogoCarousel from '@/components/LogoCarousel'
 import React from 'react'
 
+import Facebook from '/public/assets/logos/facebook.svg'
+import Disney from '/public/assets/logos/disney.svg'
+import Airbnb from '/public/assets/logos/airbnb.svg'
+import Apple from '/public/assets/logos/apple.svg'
+import Spark from '/public/assets/logos/spark.svg'
+import Samsung from '/public/assets/logos/samsung.svg'
+import Quora from '/public/assets/logos/quora.svg'
+import Sass from '/public/assets/logos/sass.svg'
+
 const LogoCarouselPage = () => {
+  const logos = [
+    { src: Facebook, alt: 'Facebook' },
+    { src: Disney, alt: 'Disney' },
+    { src: Airbnb, alt: 'Airbnb' },
+    { src: Apple, alt: 'Apple' },
+    { src: Spark, alt: 'Spark' },
+    { src: Samsung, alt: 'Samsung' },
+    { src: Quora, alt: 'Quora' },
+    { src: Sass, alt: 'Sass' },
+  ]
+
   return (
     <main className="relative min-h-screen flex flex-col justify-center bg-slate-900 overflow-hidden">
       <div className="w-full max-w-6xl mx-auto px-4 md:px-6 py-24">
         <div className="text-center">
 
-          <LogoCarousel />
+          <LogoCarousel 
+            logos={logos}
+          />
 
         </div>
       </div>
--- src/components/LogoCarousel/index.tsx
+++ src/components/LogoCarousel/index.tsx
@@ -1,25 +1,7 @@
 import Image from 'next/image'
-import Facebook from '/public/assets/logos/facebook.svg'
-import Disney from '/public/assets/logos/disney.svg'
-import Airbnb from '/public/assets/logos/airbnb.svg'
-import Apple from '/public/assets/logos/apple.svg'
-import Spark from '/public/assets/logos/spark.svg'
-import Samsung from '/public/assets/logos/samsung.svg'
-import Quora from '/public/assets/logos/quora.svg'
-import Sass from '/public/assets/logos/sass.svg'
 
-export default function LogoCarousel() {
 
-  const logos = [
-    { src: Facebook, alt: 'Facebook' },
-    { src: Disney, alt: 'Disney' },
-    { src: Airbnb, alt: 'Airbnb' },
-    { src: Apple, alt: 'Apple' },
-    { src: Spark, alt: 'Spark' },
-    { src: Samsung, alt: 'Samsung' },
-    { src: Quora, alt: 'Quora' },
-    { src: Sass, alt: 'Sass' },
-  ]
+const LogoCarousel = ({ logos }: { logos: any[] }) => {
 
   return (
     <div className="w-full inline-flex flex-nowrap overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-128px),transparent_100%)]">
@@ -40,3 +22,5 @@ export default function LogoCarousel() {
     </div>
   )
 }
+
+export default LogoCarousel;
